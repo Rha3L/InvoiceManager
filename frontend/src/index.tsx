969d984cf4599ec43b64c3bf877cedc4b653dc3c
RhaeL <raeyliang@gmail.com>
2024-04-11 12:13:11 +1000

    add dark mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./global.css";
+import ThemeContextProvider from "./context/theme.context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ThemeContextProvider>
     <App />
-  </React.StrictMode>
+  </ThemeContextProvider>
 );
