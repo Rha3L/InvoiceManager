42fc9859b5dce58c953ce6887b4e16f0c07477c7
RhaeL <raeyliang@gmail.com>
2024-04-22 12:45:37 +1000

    add loading bar

---
+++ frontend/src/components/loadingBar/LoadingBar.tsx
@@ -0,0 +1,11 @@
+import { Box, LinearProgress } from "@mui/material";
+
+const LoadingBar = () => {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgress />
+    </Box>
+  );
+};
+
+export default LoadingBar;
