b88397f39e95c8642ae18e8dfc6219c55c0aa48c
RhaeL <raeyliang@gmail.com>
2024-05-02 18:11:57 +1000

    add companyupdatedto

---
+++ backend/backend/Persistence/Dtos/Company/CompanyUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using backend.Core.Enums;
+
+namespace backend.Core.Dtos.Company
+{
+    public class CompanyUpdateDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public CompanySize Size { get; set; }
+    }
+}
