426e28f30d42dfd4c1416c18243958064b476157
RhaeL <raeyliang@gmail.com>
2024-05-06 16:36:18 +1000

    add jobupdatedto

---
+++ backend/backend/Persistence/Dtos/Job/JobUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using backend.Enums;
+
+namespace backend.Persistence.Dtos.Job
+{
+    public class JobUpdateDto
+    {
+        public string Title { get; set; } = String.Empty;
+
+        public JobLevel Level { get; set; }
+
+        public long CompanyId { get; set; }
+    }
+}
