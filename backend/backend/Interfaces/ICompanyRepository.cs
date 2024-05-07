f27904bd2b562d531772a2acdafea973465e228c
RhaeL <raeyliang@gmail.com>
2024-05-07 17:49:47 +1000

    add company repo interface

---
+++ backend/backend/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Entities;
+using backend.Persistence.Dtos.Company;
+
+namespace backend.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Task<List<Company>> GetAllAsync();
+        Task<Company?> GetByIdAsync(int id);
+        Task<Company> CreateAsync(Company companyEntity);
+        Task<Company?> UpdateAsync(int id, CompanyUpdateDto companyDto);
+        Task<Company?> DeleteAsync(int id);
+    }
+}
