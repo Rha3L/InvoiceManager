a1acb347cfbd4202387fae79e029fc23a109e234
RhaeL <raeyliang@gmail.com>
2024-05-07 16:36:05 +1000

    add and update job controllers

--- backend/backend/Controllers/JobsController.cs
+++ backend/backend/Controllers/JobsController.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
-using backend.Core.Context;
-using backend.Core.Dtos.Company;
-using backend.Core.Dtos.Job;
-using backend.Core.Entities;
+using backend.Context;
+using backend.Persistence.Dtos.Job;
+using backend.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -32,7 +31,7 @@ namespace backend.Controllers
             await _context.Jobs.AddAsync(newJob);
             await _context.SaveChangesAsync();
 
-            return Ok("Job Created Sucessfully");
+            return CreatedAtAction(nameof(GetJobById), new { id = newJob.ID }, newJob);
         }
 
         //Read
@@ -43,6 +42,68 @@ namespace backend.Controllers
             var convertedJobs = _mapper.Map<IEnumerable<JobDto>>(jobs);
 
             return Ok(convertedJobs);
+        }
+
+        //Get a job by ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<JobDto>>> GetJobById([FromRoute] int id)
+        {
+            var job = await _context.Jobs.FirstOrDefaultAsync(x => x.ID == id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            var convertedJob = _mapper.Map<JobDto>(job);
+
+            return Ok(convertedJob);
+        }
+
+        //Update
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateJob([FromRoute] int id, [FromBody] JobUpdateDto dto)
+        {
+            var job = await _context.Jobs.FirstOrDefaultAsync(x => x.ID == id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Entry(dto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            var convertedJob = _mapper.Map<JobUpdateDto>(job);
+
+            return Ok(convertedJob);
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob([FromRoute] int id)
+        {
+            var job = await _context.Jobs.FirstOrDefaultAsync(x => x.ID == id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jobs.Remove(job);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
