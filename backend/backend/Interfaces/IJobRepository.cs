bc76abdd16814d0697287b61d22946cc1e3fb5c1
RhaeL <raeyliang@gmail.com>
2024-05-07 17:50:11 +1000

    add job repo interface

---
+++ backend/backend/Interfaces/IJobRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Entities;
+using backend.Persistence.Dtos.Job;
+
+namespace backend.Interfaces
+{
+    public interface IJobRepository
+    {
+        Task<List<Job>> GetAllAsync();
+        Task<Job?> GetByIdAsync(int id);
+        Task<Job> CreateAsync(Job jobEntity);
+        Task<Job?> UpdateAsync(int id, JobUpdateDto jobDto);
+        Task<Job?> DeleteAsync(int id);
+    }
+}
