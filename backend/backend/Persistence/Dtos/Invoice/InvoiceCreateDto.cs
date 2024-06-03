b7253d40943c3f44732fcdb9084ab7ff24c71c16
RhaeL <raeyliang@gmail.com>
2024-06-03 20:35:39 +1000

    update invoice dtos

---
+++ backend/backend/Persistence/Dtos/Invoice/InvoiceCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using backend.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Persistence.Dtos.Job
+{
+    public class InvoiceCreateDto
+    {
+        public InvoiceCategory Categories { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public bool AnyGST { get; set; }
+
+        public float GST { get; set; }
+
+        public float Total { get; set; }
+
+        public float Net { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Notes cannot be over 100 characters")]
+        public string Notes { get; set; } = string.Empty;
+    }
+}
