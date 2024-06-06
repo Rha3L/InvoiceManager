6b6c4c4534a456069a7c16d876ee5a5708fa28df
RhaeL <raeyliang@gmail.com>
2024-06-06 16:22:09 +1000

    initial commit

---
+++ backend/backend/Entities/Expense.cs
@@ -0,0 +1,32 @@
+﻿using backend.Enums;
+
+namespace backend.Entities
+{
+    public class Expense: BaseEntity
+    {
+        public ExpenseCategory Categories { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool AnyGST { get; set; }
+
+        public float GST { get; set; }
+
+        public float Total { get; set; }
+
+        public float Net { get; set; }
+
+        public bool Paid { get; set; }
+
+        public string? Notes { get; set; }
+
+        //Relations
+        public long SupplierId { get; set; }
+
+        public required Supplier Supplier { get; set; }
+
+        public required User User { get; set; }
+    }
+}
