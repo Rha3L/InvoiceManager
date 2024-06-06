b187d2a29ebe23ab2f8c8bc8c836aa51ff5c81b7
RhaeL <raeyliang@gmail.com>
2024-06-06 16:20:43 +1000

    add income entity

---
+++ backend/backend/Entities/Income.cs
@@ -0,0 +1,30 @@
+﻿using backend.Enums;
+
+namespace backend.Entities
+{
+    public class Income: BaseEntity
+    {
+        public IncomeCategory Categories { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public float GST { get; set; }
+        
+        public float Total {  get; set; }
+
+        public float Net { get; set; }
+
+        public bool Paid { get; set; }
+
+        public string? Notes { get; set; }
+
+        //Relations
+        public long CustomerId { get; set; }
+        
+        public required Customer Customer { get; set; }
+
+        public required User User { get; set; }
+    }
+}
