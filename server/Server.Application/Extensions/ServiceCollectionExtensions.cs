cc3b01ec257484ecabb2267cfc9bb39c4b8a36fd
RhaeL <raeyliang@gmail.com>
2024-06-28 17:24:46 +1000

    add service extensions

---
+++ server/Server.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Server.Application.Customers;
+
+
+namespace Server.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomerService, CustomerService>();
+
+        }
+    }
+}
