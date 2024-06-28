384d09df48d117c9841c16496e9e51b4e5de630a
RhaeL <raeyliang@gmail.com>
2024-06-28 17:26:11 +1000

    add get methods

--- server/Server.API/Server.API/Controllers/CustomersController.cs
+++ server/Server.API/Server.API/Controllers/CustomersController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Server.Application.Customers;
 
 namespace Server.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/customers")]
     [ApiController]
-    public class CustomersController : ControllerBase
+    public class CustomersController(ICustomerService customerService) : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAll() 
+        { 
+            var customers = await customerService.GetAllCustomers();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCustomerById([FromRoute] int id)
+        {
+            var customer = await customerService.GetCustomerById(id);
+            if (customer == null)
+                return NotFound();
+
+            return Ok(customer);
+        }
     }
 }
