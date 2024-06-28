572520778dde1306e1a4f372a4a7ca35e18fd454
RhaeL <raeyliang@gmail.com>
2024-06-28 17:28:59 +1000

    add customer get services

---
+++ server/Server.Application/Customers/CustomerService.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Server.Domain.Entities;
+using Server.Domain.Repositories;
+
+namespace Server.Application.Customers
+{
+    internal class CustomerService(ICustomerRepository customerRepository, ILogger<CustomerService> logger) : ICustomerService
+    {
+        public async Task<IEnumerable<Customer>> GetAllCustomers()
+        {
+            logger.LogInformation("Getting all customers");
+            var customers = await customerRepository.GetAllAsync();
+            return customers;
+        }
+
+        public async Task<Customer?> GetCustomerById(int id)
+        {
+            logger.LogInformation($"Getting customer {id}");
+            var customer = await customerRepository.GetByIdAsync(id);
+            return customer;
+    }
+}
---
+++ server/Server.Application/Customers/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using Server.Domain.Entities;
+
+namespace Server.Application.Customers
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Customer>> GetAllCustomers();
+        Task<Customer?> GetCustomerById(int id);
+    }
+}
