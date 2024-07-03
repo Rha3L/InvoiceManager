2e18a37b84168ea779f20948c52a45d7ddbe09b7
RhaeL <raeyliang@gmail.com>
2024-07-03 17:51:08 +1000

    add mapping

---
+++ server/Server.Application/Customers/Dtos/CustomerProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Server.Domain.Entities;
+
+namespace Server.Application.Customers.Dtos;
+public class CustomerProfile: Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<Customer, CustomerDto>()
+            .ForMember(dest => dest.Income, opt => opt.MapFrom(src => src.Income));
+    }
+}
+
