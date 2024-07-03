2d8ae197d2a82c6733d96561b0f7ff526883ddc7
RhaeL <raeyliang@gmail.com>
2024-07-03 21:11:15 +1000

    add dto

---
+++ server/Server.Application/Suppliers/Dtos/SupplierProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Server.Domain.Entities;
+
+namespace Server.Application.Suppliers.Dtos;
+public class SupplierProfile: Profile
+{
+    public SupplierProfile()
+    {
+        CreateMap<Supplier, SupplierDto>()
+            .ForMember(dest => dest.Expenses, opt => opt.MapFrom(src => src.Expenses));
+    }
+}
+
