b0bf37c722f9dbfc1dd8f3596f5a053d3b95b17e
RhaeL <raeyliang@gmail.com>
2024-07-07 22:23:57 +1000

    add validator

---
+++ server/Server.Application/Customers/Validators/CreateCustomerDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Server.Application.Customers.Dtos;
+
+namespace Server.Application.Customers.Validators;
+
+internal class CreateCustomerDtoValidator: AbstractValidator<CreateCustomerDto>
+{
+    public CreateCustomerDtoValidator()
+    {
+        RuleFor(dto => dto.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.");
+
+        RuleFor(dto => dto.ABN)
+            .Length(11)
+            .WithMessage("Please provide a valid ABN.");
+    }
+}
