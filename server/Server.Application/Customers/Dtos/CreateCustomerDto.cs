e333a36eed97f28010909f36721151d8ffd425e4
RhaeL <raeyliang@gmail.com>
2024-07-08 15:29:26 +1000

    add create dto

---
+++ server/Server.Application/Customers/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Application.Customers.Dtos;
+
+public class CreateCustomerDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string? ABN { get; set; }
+}
---
+++ server/Server.Application/Suppliers/Dtos/CreateSupplierDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Application.Suppliers.Dtos;
+
+public class CreateSupplierDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string ABN { get; set; } = string.Empty;
+}
