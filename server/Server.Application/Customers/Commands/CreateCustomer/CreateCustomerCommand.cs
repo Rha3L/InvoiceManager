3abbbc9232e333cca2d3f9d1088ce5bf3ff27959
RhaeL <raeyliang@gmail.com>
2024-07-10 16:53:35 +1000

    add command and handler

---
+++ server/Server.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Server.Application.Customers.Commands.CreateCustomer;
+
+internal class CreateCustomerCommand: IRequest<int>
+{
+    public string Name { get; set; } = string.Empty;
+    public string? ABN { get; set; }
+}
---
+++ server/Server.Application/Customers/Commands/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Server.Domain.Entities;
+using Server.Domain.Repositories;
+
+namespace Server.Application.Customers.Commands.CreateCustomer;
+
+public class CreateCustomerCommandHandler(ILogger<CreateCustomerCommandHandler> logger, IMapper mapper, ICustomerRepository customerRepository) : IRequestHandler<CreateCustomerCommand, int>
+{
+    public async Task<int> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Creating a new customer");
+        var customer = mapper.Map<Customer>(request);
+        int id = await customerRepository.CreateCustomer(customer);
+        return id;
+    }
+}
