66df0f74177c1cc9651cc3e40a1cbf6bdaedf8e8
RhaeL <raeyliang@gmail.com>
2024-07-10 17:13:47 +1000

    fix accessibility

--- server/Server.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
+++ server/Server.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Server.Application.Customers.Commands.CreateCustomer;
 
-internal class CreateCustomerCommand: IRequest<int>
+public class CreateCustomerCommand: IRequest<int>
 {
     public string Name { get; set; } = string.Empty;
     public string? ABN { get; set; }
--- server/Server.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
+++ server/Server.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -3,7 +3,7 @@ using Server.Application.Customers.Dtos;
 
 namespace Server.Application.Customers.Queries.GetCustomerById;
 
-internal class GetCustomerByIdQuery(int id) : IRequest<CustomerDto?>
+public class GetCustomerByIdQuery(int id) : IRequest<CustomerDto?>
 {
-    public int ID { get; set; } = id;
+    public int ID { get; } = id;
 }
