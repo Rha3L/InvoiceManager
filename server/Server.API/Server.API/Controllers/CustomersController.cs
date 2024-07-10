c77f390847f1ce9876ce7d75ccddd56e7efa9593
RhaeL <raeyliang@gmail.com>
2024-07-10 17:07:09 +1000

    add mediator

--- server/Server.API/Server.API/Controllers/CustomersController.cs
+++ server/Server.API/Server.API/Controllers/CustomersController.cs
@@ -1,28 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Server.Application.Customers;
+using Server.Application.Customers.Commands.CreateCustomer;
+using Server.Application.Customers.Queries.GetAllCustomers;
+using Server.Application.Customers.Queries.GetCustomerById;
 
-namespace Server.API.Controllers
+
+namespace Server.API.Controllers;
+
+[Route("api/customers")]
+[ApiController]
+public class CustomersController(IMediator mediator) : ControllerBase
 {
-    [Route("api/customers")]
-    [ApiController]
-    public class CustomersController(ICustomerService customerService) : ControllerBase
+    [HttpGet]
+    public async Task<IActionResult> GetAllCustomers()
     {
-        [HttpGet]
-        public async Task<IActionResult> GetAll() 
-        { 
-            var customers = await customerService.GetAllCustomers();
-            return Ok(customers);
-        }
+        var customers = await mediator.Send(new GetAllCustomersQuery());
+        return Ok(customers);
+    }
 
-        [HttpGet("{id:int}")]
-        public async Task<IActionResult> GetCustomerById([FromRoute] int id)
-        {
-            var customer = await customerService.GetCustomerById(id);
-            if (customer == null)
-                return NotFound();
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetCustomerById([FromRoute] int id)
+    {
+        var customer = await mediator.Send(new GetCustomerByIdQuery(id));
+        
+        if (customer == null)
+            return NotFound();
 
-            return Ok(customer);
-        }
+        return Ok(customer);
+    }
+
+    [HttpPost]
+    [Route("create")]
+    public async Task<IActionResult> CreateCustomer(CreateCustomerCommand command)
+    {
+        int id = await mediator.Send(command);
+
+        return CreatedAtAction(nameof(GetCustomerById), new { id }, null);
     }
 }
