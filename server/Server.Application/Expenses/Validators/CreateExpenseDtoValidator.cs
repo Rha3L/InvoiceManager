ffaedc24a04b758283015966a6f9c85714a3710c
RhaeL <raeyliang@gmail.com>
2024-07-23 10:17:01 +1000

    add enum validator

---
+++ server/Server.Application/Expenses/Validators/CreateExpenseDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Server.Application.Expenses.Dtos;
+
+namespace Server.Application.Expenses.Validators;
+
+public class CreateExpenseDtoValidator: AbstractValidator<CreateExpenseDto>
+{
+    public CreateExpenseDtoValidator() 
+    {
+        RuleFor(dto => dto.Categories)
+            .IsInEnum()
+            .WithMessage("Invalid category. Please choose from the valid categories.S");
+    }
+}
