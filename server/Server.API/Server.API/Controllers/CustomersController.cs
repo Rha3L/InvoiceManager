7e199209f44f37a3dae369be611436b80e41b871
RhaeL <raeyliang@gmail.com>
2024-07-23 10:21:55 +1000

    update controller with mediator

--- server/Server.API/Server.API/Controllers/CustomersController.cs
+++ server/Server.API/Server.API/Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Server.Application.Customers.Commands.CreateCustomer;
 using Server.Application.Customers.Queries.GetAllCustomers;
