db8568b91456f1dbd076256a0068d782f761ec6f
RhaeL <raeyliang@gmail.com>
2024-07-23 10:18:21 +1000

    remove files

--- server/Server.Application/Suppliers/Dtos/CreateSupplierDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Application.Suppliers.Dtos;
-
-public class CreateSupplierDto
-{
-    public string Name { get; set; } = string.Empty;
-
-    public string ABN { get; set; } = string.Empty;
-}
