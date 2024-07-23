c65a6da1f11551b50d2a41e453a684f5aadf02d4
RhaeL <raeyliang@gmail.com>
2024-07-23 11:32:20 +1000

    add get all query

---
+++ server/Server.Application/Suppliers/Queries/GetAllSuppliers/GetAllSuppliersQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Server.Application.Suppliers.Dtos;
+
+namespace Server.Application.Suppliers.Queries.GetAllSuppliers;
+
+public class GetAllSuppliersQuery : IRequest<IEnumerable<SupplierDto>>
+{
+}
---
+++ server/Server.Application/Suppliers/Queries/GetAllSuppliers/GetAllSuppliersQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Server.Application.Suppliers.Dtos;
+using Server.Domain.Repositories;
+
+namespace Server.Application.Suppliers.Queries.GetAllSuppliers;
+
+public class GetAllSuppliersQueryHandler(ILogger<GetAllSuppliersQueryHandler> logger, IMapper mapper, ISupplierRepository supplierRepository) : IRequestHandler<GetAllSuppliersQuery, IEnumerable<SupplierDto>>
+{
+    public async Task<IEnumerable<SupplierDto>> Handle(GetAllSuppliersQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Getting all suppliers");
+        var suppliers = await supplierRepository.GetAllAsync();
+        var convertedSuppliers = mapper.Map<IEnumerable<SupplierDto>>(suppliers);
+        return convertedSuppliers;
+    }
+}
