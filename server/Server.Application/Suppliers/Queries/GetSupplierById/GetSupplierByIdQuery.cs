48a5a4452e91fd60cec1c13ebfba52bd2c3e9ca3
RhaeL <raeyliang@gmail.com>
2024-07-23 11:33:30 +1000

    add get by id query

---
+++ server/Server.Application/Suppliers/Queries/GetSupplierById/GetSupplierByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Server.Application.Suppliers.Dtos;
+
+namespace Server.Application.Suppliers.Queries.GetSupplierById;
+
+public class GetSupplierByIdQuery(int id) : IRequest<SupplierDto?>
+{
+    public int ID { get; } = id;
+}
---
+++ server/Server.Application/Suppliers/Queries/GetSupplierById/GetSupplierByIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Server.Application.Suppliers.Dtos;
+using Server.Domain.Repositories;
+
+namespace Server.Application.Suppliers.Queries.GetSupplierById;
+
+public class GetSupplierByIdQueryHandler(ILogger<GetSupplierByIdQueryHandler> logger, IMapper mapper, ISupplierRepository supplierRepository) : IRequestHandler<GetSupplierByIdQuery, SupplierDto?>
+{
+    public async Task<SupplierDto?> Handle(GetSupplierByIdQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Getting supplier {request.ID}");
+        var supplier = await supplierRepository.GetByIdAsync(request.ID);
+        var convertedSupplier = mapper.Map<SupplierDto>(supplier);
+        return convertedSupplier;
+    }
+}
