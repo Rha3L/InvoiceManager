9193d5209ad5a707b1ee19b8425cd17a42023e82
RhaeL <raeyliang@gmail.com>
2024-07-23 11:42:56 +1000

    add service collections

---
+++ server/Server.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Domain.Repositories;
+using Server.Infrastructure.Repositories;
+
+namespace Server.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ISupplierRepository, SupplierRepository>();
+            services.AddScoped<IIncomeRepository, IncomeRepository>();
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+
+        }
+    }
+}
